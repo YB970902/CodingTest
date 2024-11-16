07abcfb0739da8dcd9481eb411508cc14693164b
Lailael <78996787+YB970902@users.noreply.github.com>
2024-11-17 01:07:40 +0900

    [Bronze I] Title: 이항 계수 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.cc
@@ -0,0 +1,56 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+unsigned long long fact(unsigned long long val)
+{
+    if(val == 1) return 1;
+    
+    return val * fact(val - 1);
+}
+
+int main()
+{
+    int N, K;
+    cin >> N >> K;
+    
+    if(N == K)
+    {
+        cout << 1 << endl;
+        return 0;
+    }
+    
+    if(K == 1)
+    {
+        cout << N << endl;
+        return 0;
+    }
+    
+    if(K == 0)
+    {
+        cout << 1 << endl;
+        return 0;
+    }
+    
+    if(K >= N - K)
+    {
+        unsigned long long n1 = 1;
+        for(unsigned long long j = K + 1; j <= N; ++j)
+        {
+            n1 *= j;
+        }
+        
+        cout << n1 / fact(N - K);
+    }
+    else
+    {
+        unsigned long long n1 = 1;
+        for(unsigned long long j = N - K + 1; j <= N; ++j)
+        {
+            n1 *= j;
+        }
+        
+        cout << n1 / fact(K);
+    }
+}
