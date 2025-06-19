6c6538e62ec94438d7d3fa699077acfc9e04b80a
Lailael <78996787+YB970902@users.noreply.github.com>
2025-06-19 15:15:39 +0900

    [Silver II] Title: 랜선 자르기, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.cc
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+// 랜선의 개수
+int K;
+// 필요한 랜선의수
+int N;
+// 랜선의 개수별 길이
+vector<long long> V;
+
+bool isPossible(const long long& len)
+{
+    long long result = 0;
+    for (int i = 0; i < K; ++i)
+    {
+        result += V[i] / len;
+    }
+
+    return result >= N;
+}
+
+int main()
+{
+    cin >> K >> N;
+
+    V.resize(K);
+
+    for (int i = 0; i < K; ++i)
+    {
+        cin >> V[i];
+    }
+
+    // 자를 수 있는 가장 큰 길이
+    long long high = *max_element(V.begin(), V.end());
+    // 자를 수 있는 가장 작은 길이
+    long long low = 1;
+
+    while (low < high - 1)
+    {
+        // 중심값
+        long long mid = (low + high) / 2;
+
+        if (isPossible(mid))
+        {
+            low = mid;
+        }
+        else
+        {
+            high = mid - 1;
+        }
+    }
+
+    if (isPossible(low + 1)) cout << low + 1;
+    else cout << low;
+
+    return 0;
+}
